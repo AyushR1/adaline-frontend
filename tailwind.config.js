15358c5ce2cb707d96cf3d7d5ab3e11abf6722d2
AyushR1 <ayushrathore12501@gmail.com>
2024-12-12 21:25:06 +0530

    Init react
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
